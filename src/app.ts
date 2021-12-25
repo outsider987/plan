8e7f736f65ae08324282cb7ce44ac53eafc3ba2d
victor <victor.paradromix@gmail.com>
2021-12-25 20:14:12 +0800

    backend: 1.add database 2.set up debug file 3. add env

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { router } from "./router";
+import mysql from "mysql";
+import {db} from "@/database/sqlConnect"
 
 const app: express.Application = express();
 const port = 9000;
+debugger
+db.connect();
 // add router
 for (const route of router) {
   app.use(route.getRouter());
