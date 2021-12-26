0565221b4c803b377782c0e70ba7494a7cf598d2
Outsider987 <t7902195204@gmail.com>
2021-12-26 19:07:50 +0800

    backend: add Access-Control-Allow-Origin header in express

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,26 @@ import { router } from "./router";
 import mysql from "mysql";
 import { db } from "@/database/sqlConnect";
 import dotenv from "dotenv";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
+
+// settings
 const swaggerDocument = require("./swagger_output.json");
 dotenv.config();
+const corsOptions = {
+  origin: ["http://www.example.com", "http://localhost:3000"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Origin",
+  ],
+};
 
 const app: express.Application = express();
 app.use(express.json());
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors(corsOptions));
 const port = 4000;
 
 // add router
